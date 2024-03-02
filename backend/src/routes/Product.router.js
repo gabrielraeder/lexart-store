70eef86cc4235a18a17b9b754655e486d7ea78a4
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2024-03-02 20:59:09 -0300

    refactor: changes post routes to be /

--- backend/src/routes/Product.router.js
+++ backend/src/routes/Product.router.js
@@ -10,6 +10,6 @@ const router = express.Router();
 router.get('/', (req, res, next) => new ProductController(req, res, next).getAll());
 router.get('/:id', (req, res, next) => new ProductController(req, res, next).getById());
 router.put('/:id', (req, res, next) => new ProductController(req, res, next).update());
-router.post('/create', (req, res, next) => new ProductController(req, res, next).create());
+router.post('/', (req, res, next) => new ProductController(req, res, next).create());
 
 module.exports = router;
