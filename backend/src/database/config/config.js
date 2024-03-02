462bd13499579cb0b57b07d4d5ca882bca2992fc
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2024-03-02 14:44:34 -0300

    fix: database dialect options

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -5,9 +5,15 @@ module.exports = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    dialectModule: require('pg')
+    dialectModule: require('pg'),
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.POSTGRES_USER,
