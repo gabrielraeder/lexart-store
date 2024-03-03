c9451e5011c35870de21519c1806f14d4ec4a27e
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2024-03-02 21:00:40 -0300

    refactor: refactors to simplify product mapping

--- backend/src/utils/productMap.js
+++ backend/src/utils/productMap.js
@@ -1,41 +1,26 @@
 const productMap = (data) => {
+  const product = {
+    name: data.name,
+    brand: data.brand,
+    model: data.model
+  };
+
   if (Object.keys(data).includes('details')) {
     return {
-      product: {
-        name: data.name,
-        brand: data.details.brand,
-        model: data.details.model
-      },
-      details: [
-        {
-          price: data.price,
-          color: data.details.color
-        }
-      ]
-    }
+      product: { ...product, ...data.details },
+      details: [{ price: data.price, color: data.details.color }]
+    };
   } else if (Object.keys(data).includes('data')) {
     return {
-      product: {
-        name: data.name,
-        brand: data.brand,
-        model: data.model
-      },
+      product: product,
       details: data.data.map((item) => ({ price: item.price, color: item.color }))
-    }
+    };
   }
+
   return {
-    product: {
-      name: data.name,
-      brand: data.brand,
-      model: data.model
-    },
-    details: [
-      {
-        price: data.price,
-        color: data.color
-      }
-    ]
-  }
+    product: product,
+    details: [{ price: data.price, color: data.color }]
+  };
 }
 
 module.exports = productMap;
